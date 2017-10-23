9de85ec1efa92f155e4304d6102b6b61542e9adb
tkataras <31904698+tkataras@users.noreply.github.com>
2017-10-23 16:22:24 -0700

    Update bin_script.R

--- bin_script.R
+++ bin_script.R
@@ -134,11 +134,7 @@ binit <- function(datain="~/bigdata/hic/data/SRR2240738/mm10/output/hic_results/
     
      
      
-  #    verify2 <- binit_out$bin_rd_pos_1 > binit_out$bin_rd_pos_2
-  # sum(verify2)#this doesnt seem to be working, the switching
-  # 
-  # head(binit_out) ###rd_pos not adding index sufficinetly
-  # 
+  
 
     
    ##### selecting all rows in binit_out within unique pair of bins
@@ -204,18 +200,6 @@ binit <- function(datain="~/bigdata/hic/data/SRR2240738/mm10/output/hic_results/
 
   
   
-    # #BINNING
-  # datain <- read.table(datain)
-  # colnames(datain) <-  c("read_name", "ch_1_locus", "rd_pos_1", "strand1", "ch_2_locus", "rd_pos_2", "strand2", "frag_lngth","res_frag_1", "res_frag_2", "map_qual_1", "map_qual_2", "chrom_assign")
-  # 
-  # bin_rd_pos_1_no_round <- datain$rd_pos_1/sbin
-  # bin_rd_pos_1 <- floor(bin_rd_pos_1_no_round) #using floor rather than ceiling to start at bin 0
-  #   
-  # bin_rd_pos_2_no_round <- datain$rd_pos_2/sbin
-  # bin_rd_pos_2 <- floor(bin_rd_pos_2_no_round)
-  # binit_out<-data.frame(datain$read_name, datain$ch_1_locus, datain$rd_pos_1, bin_rd_pos_1, datain$ch_2_locus, datain$rd_pos_2, bin_rd_pos_2, datain$chrom_assign, row.names=TRUE)
-  # 
-  ####OUTPUT
   
 }
 
